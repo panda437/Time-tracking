328a27ca0acb6c8257e5201c8d98046c08c48f93
panda437 <asifkabeer1@gmail.com>
2025-07-09 23:02:55 +0530

    fix: use Clarity.init API

--- src/components/MicrosoftClarity.tsx
+++ src/components/MicrosoftClarity.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 import { useEffect } from "react"
-// @ts-expect-error clarity has no types
-import clarity from "@microsoft/clarity"
+// @ts-expect-error clarity no types default object
+import Clarity from "@microsoft/clarity"
 
 export default function MicrosoftClarity() {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      clarity("init", "scas5wbamy")
+      Clarity.init("scas5wbamy")
     }
   }, [])
   return null
